ee7a14ae5a6a80dcd049009311d4970062904db4
peehu16 <pooja.yadav@mavs.uta.edu>
2015-04-19 14:00:40 -0500

    function

--- sunday_pm_gapminder.R
+++ sunday_pm_gapminder.R
@@ -1,4 +1,26 @@
 #this is a file to explore gapminder dataset using R
 #19 april 2015
-gap.in <- read.table("output/combined_gapMinder.tsv"sep= "\t")
+gap.in <- read.table("output/combined_gapMinder.tsv", sep="\t", head =T)
 
+for (number in 1:10) {
+  print (number + 10)
+}
+
+for (number in 1:10){
+  number.out <- (number + 10)
+}
+
+for (number in 1:10){
+  print(gap.in[number])}
+
+for (row.number in 1:10){
+  for (col.number in 1:5){
+    print(gap.in[row.number,col.number])
+  }
+}
+
+add.me <- function(x,y){
+  x+y
+}
+
+add.me(3, 4)
